85c4d5eb1fc46abcf107361ee139de00451267ed
Gennady Guniashev <genateepspb@gmail.com>
2022-01-23 14:04:53 +0300

    upd

--- statApp/views.py
+++ statApp/views.py
@@ -36,22 +36,54 @@ class IndexPage(TemplateView):
         post_date = kwargs['request'].POST or None
 
         if post_date:
-            stat = OneDayData.objects.filter(
-                city=post_date['city'],
-                date__range=[post_date['start_date'], post_date['end_date']]
-            ).aggregate(
-                day_count=Count('date'),
-                abs_min_temp=Min('minTemp'),
-                avg_temp=Avg('avgTemp'),
-                abs_max_temp=Max('maxTemp'),
-                precip_days=Count('precipitation', filter=Q(precipitation=0)),
-                description='desc',
-                avg_wind_speed=Avg('windSpeed'),
-                wind_dir='windDir'
+            city = post_date['city']
+            start_date = post_date['start_date']
+            end_date = post_date['end_date']
+
+            raw_data = OneDayData.objects.filter(
+                city=city,
+                date__range=[start_date, end_date]
             )
 
-            # calculating % of precip days
-            stat['precip_days'] = str(round(stat['precip_days'] / stat['day_count'] * 100)) + ' %'
+            # .aggregate(
+            #     day_count=Count('date'),
+            #     abs_min_temp=Min('minTemp'),
+            #     avg_temp=Avg('avgTemp'),
+            #     abs_max_temp=Max('maxTemp'),
+            #     precip_days=Count('precipitation', filter=Q(precipitation=0)),
+            #     avg_wind_speed=Avg('windSpeed'),
+            # )
+            stat = {}
+
+            stat['city'] = city
+            stat['start_date'] = raw_data.first().date
+            stat['end_date'] = raw_data.last().date
+            stat['day_count'] = raw_data.aggregate(Count('date'))
+
+            stat['abs_min_temp'] = raw_data.aggregate(Min('minTemp'))['minTemp__min']
+            stat['avg_temp'] = raw_data.aggregate(Avg('avgTemp'))['avgTemp__avg']
+            stat['abs_max_temp'] = raw_data.aggregate(Max('maxTemp'))['maxTemp__max']
+
+            if int(start_date[:4]) < int(end_date[:4]) + 2:
+                stat['year_min'] = raw_data.values('date__year').annotate(
+                    Avg('minTemp')
+                ).order_by("date__year")[0]['minTemp__avg']
+
+                stat['year_max'] = raw_data.values('date__year').annotate(
+                    Avg('maxTemp')
+                ).order_by('date__year')[0]['maxTemp__avg']
+
+            stat['same_temp_day'] =
+
+            days_zero_prec = raw_data.annotate(Count('precipitation')).filter(precipitation=0).count()
+            stat['precip_days'] = round(days_zero_prec / raw_data.annotate(ays=Count('precipitation')).count() * 100)
+
+            desc_list = [x['desc'] for x in raw_data.values('desc').annotate(count=Count('desc')).order_by('-count')[:2]]
+            stat['most_frequent_prec'] = ', '.join(desc_list)
+
+            stat['avg_wind_speed'] = raw_data.aggregate(Avg('windSpeed'))['windSpeed__avg']
+
+            stat['avg_wind_dir'] = raw_data.aggregate(Avg('windDir'))['windDir__avg']
 
             context['stat'] = stat
 
